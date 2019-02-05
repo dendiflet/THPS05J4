0c1d47dc3e53782553962d616d2e27a26648ebf9
dendiflet <bretin.iannis@gmail.com>
2019-02-05 22:56:24 +0100

    avec les redirection

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,11 +13,22 @@ class GossipsController < ApplicationController
     #byebug
     puts "$" * 60
     puts "création d'un nouveau gossip"
-    @new_one = Gossip.create!(title: params[:gossip][:title], content: params[:gossip][:content], user_id: params[:gossip][:user_id])
+    @new_one = Gossip.new(title: params[:gossip][:title], content: params[:gossip][:content], user_id: params[:gossip][:user_id])
+    
+  if @new_one.save # essaie de sauvegarder en base @gossip
+    # si ça marche, il redirige vers la page d'index du site
     puts "$" * 20 + "  affichage du gossip"		
 		puts @new_one
+     redirect_to root_url
+  else
+    # sinon, il render la view new (qui est celle sur laquelle on est déjà)
     puts "$" * 60  	
- redirect_to root_url
+    puts "########   ERREUR   ###############"
+    redirect_to new_gossip_url
+  end
+
+
+
   end
 
 
